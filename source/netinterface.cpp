f8a4df3d1a673fc103e6401198202493c8601c23
ravi688 <rp0412204@gmail.com>
2025-09-27 04:51:38 +0530

    Added implementation for windows as well

--- source/netinterface.cpp
+++ source/netinterface.cpp
@@ -1,9 +1,15 @@
 #include <netsocket/netinterface.hpp>
 #include <netsocket/assert.hpp>
 
-#include <ifaddrs.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#ifdef PLATFORM_WINDOWS
+#	include <winsock2.h>
+#	include <iphlpapi.h>
+#	include <ws2tcpip.h>
+#else // PLATFORM_LINUX
+#	include <ifaddrs.h>
+#	include <arpa/inet.h>
+#	include <netinet/in.h>
+#endif
 
 #include <ranges>
 #include <string>
@@ -25,6 +31,44 @@ namespace netsocket
 	{
 		std::vector<std::pair<std::string, IPv4Address>> addresses;
 
+#ifdef PLATFORM_WINDOWS
+		ULONG outBufLen = 15000; // large enough buffer
+		PIP_ADAPTER_ADDRESSES pAddresses = (IP_ADAPTER_ADDRESSES*)malloc(outBufLen);
+		if(!pAddresses)
+		    throw std::runtime_error("Memory allocation failed for IP_ADAPTER_ADDRESSES");
+
+		DWORD dwRet = GetAdaptersAddresses(AF_INET, 0, nullptr, pAddresses, &outBufLen);
+		if(dwRet != NO_ERROR)
+		{
+    		free(pAddresses);
+    		throw std::runtime_error("GetAdaptersAddresses failed");
+		}
+
+    	for(PIP_ADAPTER_ADDRESSES pCurr = pAddresses; pCurr != nullptr; pCurr = pCurr->Next)
+    	{
+        	for(PIP_ADAPTER_UNICAST_ADDRESS pUnicast = pCurr->FirstUnicastAddress; pUnicast != nullptr; pUnicast = pUnicast->Next) 
+        	{
+        	    SOCKADDR* addr = pUnicast->Address.lpSockaddr;
+        	    if(addr->sa_family == AF_INET)
+        	    {
+        	        char ip[INET_ADDRSTRLEN];
+        	        inet_ntop(AF_INET, &((struct sockaddr_in*)addr)->sin_addr, ip, sizeof(ip));
+
+					std::string s { ip };
+					IPv4Address ipAddress;
+					for(uint32_t i = 0; auto part : std::views::split(s, '.'))
+						ipAddress[i++] = std::stoul(std::string { part.begin(), part.end() });
+
+					// Convert wide FriendlyName -> narrow string
+					std::wstring wname = pCurr->FriendlyName;
+					std::string ifName(wname.begin(), wname.end());
+
+					addresses.emplace_back(ifName, ipAddress);
+				}
+			}
+		}
+		free(pAddresses);
+#else // PLATFORM_LINUX
 		struct ifaddrs* ifaddr;
 		char ip[INET_ADDRSTRLEN];
 
@@ -49,7 +93,7 @@ namespace netsocket
 		}
 		
 		freeifaddrs(ifaddr);
-
+#endif
 		return addresses;
 	}
 
@@ -58,8 +102,10 @@ namespace netsocket
 		auto it = std::ranges::find_if(ipAddresses, [](const auto& pair)
                         {
 				const std::string& interfaceName = pair.first;
-#if PLATFORM_WINDOWS
-                                return interfaceName.starts_with("Ethernet adapter") || interfaceName.starts_with("Wireless LAN");
+#ifdef PLATFORM_WINDOWS
+                                return interfaceName.starts_with("Ethernet") ||
+                                		interfaceName.starts_with("Wireless LAN") ||
+                                		interfaceName.starts_with("Network Bridge");
 #else // if PLATFORM_LINUX
                                 return interfaceName.starts_with("en") ||
                                 	interfaceName.starts_with("eth") ||
